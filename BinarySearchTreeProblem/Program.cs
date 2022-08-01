74ce9cc9fafb169944c3e0ad83dee1d8a96b9fa3
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-08-01 20:42:04 +0530

    Add project files.

---
+++ BinarySearchTreeProblem/Program.cs
@@ -0,0 +1,7 @@
+﻿using BinarySearchTreeProblem;
+
+BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+binarySearch.Insert(30);
+binarySearch.Insert(70);
+binarySearch.Display();
+binarySearch.GetSize();
